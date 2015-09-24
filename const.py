8d643c116104722b64c8e749a47eab2e24a81529
xiangrong zeng <xiangrongzeng@foxmail.com>
2015-09-24 22:38:54 +0800

    添加了路径常量

---
+++ const.py
@@ -0,0 +1,8 @@
+#! coding = utf-8
+
+class FilePath:
+    dim_items = "c:\Users\SunderLab\Documents\myfile\cloth_matching_challenge\data\dim_items.txt"
+    dim_fashion_matchsets = \
+        "c:\Users\SunderLab\Documents\myfile\cloth_matching_challenge\data\dim_fashion_matchsets.txt"
+    test_items = "c:\Users\SunderLab\Documents\myfile\cloth_matching_challenge\data\\test_items.txt"
+    fm_submissions = "c:\Users\SunderLab\Documents\myfile\cloth_matching_challenge\data\dfm_submissions.txt"
